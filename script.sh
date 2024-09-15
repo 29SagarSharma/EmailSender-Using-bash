c6745e24892fcc7b9eb057343835d6635749d05b
Sagar sharma <sagaramps29@gmail.com>
2024-09-15 18:23:55 +0530

    Basic GUI

--- script.sh
+++ script.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-NAME=$(whiptail --inputbox "Yourname" 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
-DATE=$(whiptail --inputbox "Date" 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
-STATUS=$(whiptail --inputbox "Status" 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
 
-                                                                        # A trick to swap stdout and stderr.
-# Again, you can pack this inside if, but it seems really long for some 80-col terminal users.
+#Using whiptail for USer input
+NAME=$(whiptail --inputbox "Yourname" 8 39 --title "Status Update" 3>&1 1>&2 2>&3)
+DATE=$(whiptail --inputbox "Date" 8 39 --title "Status Update" 3>&1 1>&2 2>&3)
+STATUS=$(whiptail --inputbox "Status" 8 39 --title "Status Update" 3>&1 1>&2 2>&3)
+
+                                                                        
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo " $NAME ":" $DATE ": Status:" $STATUS "
+    echo " $NAME ":" $DATE ": Status" $STATUS " > script.txt  
+    
 else
     echo "User selected Cancel."
 fi
+recipient="sagaramps29@gmail.com"
+subject="Status Update"
+body="Today's Status"
+attachment="script.txt"
+
+echo -e "$body" | mailx -s "$subject" -A "$attachment" "$recipient"
+    echo "Email sent successfully with attachment: $attachment"
+
+
+
 
 
